3070d6d2585354bc67bbcfe52f3633d10ef6735f
linqiang69672187 <69672187@qq.com>
2017-10-25 10:33:49 +0800

    加入版本控制

---
+++ app/src/main/java/com/eastcom/linqiang_f/coolweather/db/City.java
@@ -0,0 +1,10 @@
+package com.eastcom.linqiang_f.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by linqiang_f on 2017/10/25.
+ */
+
+public class City extends DataSupport {
+}
--- app/src/main/java/com/eastcom/linqiang_f/coolweather/db/Province.java
+++ app/src/main/java/com/eastcom/linqiang_f/coolweather/db/Province.java
@@ -1,10 +1,12 @@
 package com.eastcom.linqiang_f.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 /**
  * Created by linqiang_f on 2017/10/25.
  */
 
-public class Province {
+public class Province extends DataSupport {
     private int id;
     private String provinceName;
     private int provinceCode;
